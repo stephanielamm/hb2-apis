e201097106fb9e91e373a22407f3d9b32930fa64
stephanielamm <slamm5@live.unc.edu>
2016-04-21 12:37:10 -0400

    map, styling video

--- index.php
+++ index.php
@@ -1,42 +1,68 @@
 
 <html>
 <head>
+  <meta name="viewport" content="initial-scale=1.0">
+  <meta charset="utf-8">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Instagram and Flickr API Homework</title>
+<title>HB2</title>
   <script src="http://codeorigin.jquery.com/jquery-2.0.3.min.js" type="text/javascript"></script>
   <script src="instagram.js" type="text/javascript"></script>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   <link href="style.css" rel="stylesheet">
+  <style>
+    html, body {
+      height: 50%;
+      margin: 30;
+      padding: 30;
+    }
+    #map {
+      height: 50%;
+    }
+  </style>
 </head>
 
 <body>
 
   <div class="container-fluid">
     <div class="row header">This is the header space
-    </div>
-    <div class="row">
-      short informational paragraph goes here
-    </div>
 
-    <div class="row">
-      infographic from 182 goes here
-    </div>
     <div class="row">
       <div class="col-xs-12">
-        <iframe width="640" height="360" src="https://www.youtube.com/embed/4S5eDb3DEME" frameborder="0" allowfullscreen style="display:block; margin: 0 auto;"></iframe>
+        <iframe width="100%" height="750" src="https://www.youtube.com/embed/4S5eDb3DEME" frameborder="0" allowfullscreen style="display:block; margin: 0 auto;"></iframe>
 
 </div>
+</div>
+</div>
+
+<div class="row">
+  short informational paragraph goes here
+</div>
+
+<div class="row">
+  infographic from 182 goes here
 </div>
 
     <div class="row">
-      Map goes here
+    <div id="map" class="col-xs-4">
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDJTGfgRzYN1jJhpYcOaJnSUkSF234psEA&callback=initMap"
+    async defer></script>
+    </div>
     </div>
 
     <div class="row">
       <div class="col-xs-4">news</div>
+
       <div class="col-xs-4">Twitter
-        <script>
         <?php
         ini_set('display_errors', 1);
         require_once('TwitterAPIExchange.php');
@@ -91,14 +117,17 @@
 
         }
         ?>
-</script>
       </div>
+
+
       <div class="col-xs-4">
         <div class="head"><span style="">Instagram</span></div>
         <div id="results"></div>
       </div>
 
     </div>
+
+
     <div class="row footer">This is the footer space
     </div>
 
